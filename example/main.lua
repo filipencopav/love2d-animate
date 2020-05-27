90f6d1187fb1788b6b51329ed920c503e3b84567
Pavel Filipenco <filipencop@mail.ru>
2020-05-28 01:11:35 +0300

    updated example

--- example/main.lua
+++ example/main.lua
@@ -4,7 +4,10 @@ local animate = require "animate"
 local animation = animate("sprites.png", 16, 16, 16, 16, 4)
 
 function love.draw()
+	love.graphics.push()
+	love.graphics.scale(4)
 	animation:drawFrame(100, 100)
+	love.graphics.pop()
 end
 
 -- basic timer, calls animation:next every now and then
