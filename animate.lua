1163c8f34def1beae9145a08b6a243df1eb11eff
Pavel Filipenco <filipencop@mail.ru>
2020-05-28 01:11:01 +0300

    added aditional functionality

--- animate.lua
+++ animate.lua
@@ -7,6 +7,10 @@ local function animate(imagePath, x, y, width, height, framesCount)
 	animation.framesCount = framesCount
 	animation.x = x
 	animation.y = y
+	animation.sx = 1
+	animation.sy = 1
+	animation.fx = 1
+	animation.fy = 1
 	animation.width = width
 	animation.height = height
 	animation.image = image
@@ -18,8 +22,22 @@ local function animate(imagePath, x, y, width, height, framesCount)
 		self.quad:setViewport(self.currFrame * width + x, y, width, height, image:getDimensions())
 	end
 
-	function animation:drawFrame(px, py)
-		love.graphics.draw(self.image, self.quad, px, py)
+	function animation:drawFrame(x, y)
+		love.graphics.draw(self.image, self.quad, x, y, 0, self.sx * self.fx, self.sy * self.fy)
+	end
+
+	function animation:drawFrameCentered(x, y)
+		love.graphics.draw(self.image, self.quad, x, y, 0, self.sx * self.fx, self.sy * self.fy, self.width/2, self.height/2)
+	end
+
+	function animation:setScale(x, y)
+		self.sx = x or self.sx
+		self.sy = y or self.sy
+	end
+
+	function animation:setFlip(x, y)
+		self.fx = x or self.fx
+		self.fy = y or self.fy
 	end
 
 	return animation
